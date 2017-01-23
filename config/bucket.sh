1b2addbe24abba740395fc8fb783fec32cfa0502
Jose Riguera <jriguera@gmail.com>
2017-01-23 15:51:00 +0100

    enable public read access to the bucket

--- config/bucket.sh
+++ config/bucket.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# Create the bucket for blobs
-
+# Create the bucket for blobs and setup public read access
 BUCKET=ansible-boshrelease
 
-#s3cmd mb "s3://${BUCKET}"
-#s3cmd setacl "s3://${BUCKET}"  --acl-public --recursive
+s3cmd mb "s3://${BUCKET}"
+s3cmd setacl "s3://${BUCKET}"  --acl-public --recursive
